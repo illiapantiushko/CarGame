6a8c3aad3bd78a9401612628cdf3e9639790571d
Pantiushko Illia <illia.pantiushko@oa.edu.ua>
2020-12-10 13:32:23 +0200

    commit

---
+++ Assets/Scripts/Game/Controllers/MenuUIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public void onPressPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void onPressOptions()
+    {
+        mainMenu.gameObject.active = false;
+        optionsMenu.gameObject.active = true;
+    }
+
+    public void onPressOptionsBack()
+    {
+        mainMenu.gameObject.active = true;
+        optionsMenu.gameObject.active = false;
+    }
+}
